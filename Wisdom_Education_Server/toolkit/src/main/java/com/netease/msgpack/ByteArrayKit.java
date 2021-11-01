b5cb17b5ddf3512bc2423d04f66f58a5e378fd3b
weiliang <weiliang8080@outlook.com>
2021-11-01 17:24:16 +0800

    client-sample

--- Wisdom_Education_Server/toolkit/src/main/java/com/netease/msgpack/ByteArrayKit.java
+++
@@ -1,9 +0,0 @@
-package com.netease.msgpack;
-
-public class ByteArrayKit {
-    public static final byte[] EMPTY = new byte[0];
-
-    public static boolean isEmpty(byte[] source) {
-        return source == null || source.length == 0;
-    }
-}
